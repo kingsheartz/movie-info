8e6e45dfbdb17169d4bf32d810b178f93c39471e
kingsheartz <govind.das279@gmail.com>
2023-01-04 16:33:40 +0530

    Workflow update

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -1,8 +1,9 @@
 local_path=$1
 bucket_name=$2
+aws_key=$3
 
 rm -rf $local_path
 
 pip3 install boto3
 
-python scripts/pipeline/upload_file_to_s3.py $local_path $bucket_name
+python scripts/pipeline/upload_file_to_s3.py $local_path $bucket_name $aws_key
--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -4,13 +4,15 @@ import sys
 def main():
     local_path=sys.argv[1]
     bucket_name=sys.argv[2]
+    aws_key=sys.argv[3]
 
     session = boto3.Session()
     client = session.client('s3')
 
     response = client.upload_file(
         Filename=local_path,
-        Bucket=bucket_name
+        Bucket=bucket_name,
+        Key=aws_key
     )
     print ('Done uploading')
 
